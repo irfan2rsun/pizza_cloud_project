dd4473c4918ce81eb860f39eb30dcc1531d83fd4
Irfan Tursun <149962565+irfan2rsun@users.noreply.github.com>
2024-04-26 01:28:44 -0700

    View is still not showing anything after clicking on the submit, updated OrderController using the 'stream()' to find the pizzaId and added @PathVariable for the UUID pizzaId

--- src/main/java/com/cydeo/controller/OrderController.java
+++ src/main/java/com/cydeo/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.exception.PizzaNotFoundException;
 import com.cydeo.model.Pizza;
 import com.cydeo.model.PizzaOrder;
 import com.cydeo.repository.PizzaRepository;
@@ -33,7 +34,7 @@ public class OrderController {
     }
 
     @PostMapping("/{pizzaId}")
-    public String processOrder(UUID pizzaId, @ModelAttribute PizzaOrder pizzaOrder) {
+    public String processOrder(@PathVariable UUID pizzaId, @ModelAttribute PizzaOrder pizzaOrder) {
 
         // Saving the order
         pizzaOrder.setPizza(getPizza(pizzaId));
@@ -43,9 +44,12 @@ public class OrderController {
     }
 
     //TODO complete method
-    private Pizza getPizza(UUID pizzaId) {
+    private Pizza getPizza(UUID pizzaId) throws PizzaNotFoundException {
         // Get the pizza from repository based on it's id
-        return new Pizza();
+        return pizzaRepository.readAll().stream().filter(p -> p.getId().equals(pizzaId))
+                .findFirst().orElseThrow(() -> new PizzaNotFoundException("Pizza not found"));
+
+
     }
 
 }
